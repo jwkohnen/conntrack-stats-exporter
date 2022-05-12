fe61097323fc734b607af88e5bba4591201efa9e
Florian Domain <f.domain@criteo.com>
2022-05-12 14:52:31 +0200

    reorder project structure
    * use standard Golang structure (main entry point in /cmd, libs in /lib)

--- cmd/conntrack-stats-exporter/main.go
+++ cmd/conntrack-stats-exporter/main.go
@@ -33,7 +33,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 
-	"github.com/jwkohnen/conntrack-stats-exporter/exporter"
+	"github.com/jwkohnen/conntrack-stats-exporter/lib/exporter"
 )
 
 func main() {
