2a436174e8f66b8548ed0973f7fe0c54710e4255
Johannes Kohnen <joh-github@joh.dev>
2022-08-27 23:20:05 +0200

    Run race detecor on linux/amd64 only
    The Go race detector is not well supported on ARM.  On RaspiPi it fails due to
    incompatible virtual memory address configuration of the kernel.  On github's
    actions it fails with /go/bin/as: No such file or directory

--- build.sh
+++ build.sh
@@ -2,17 +2,19 @@
 set -eux
 
 go mod verify 
-go test --race ./...
-
-export CGO_ENABLED=0
-export GOOS=linux
 
 if [ "$(lscpu | grep Architecture | awk '{print $2}')" = "aarch64" ]; then
     echo "arm64"
     export GOARCH=arm64
+
+    go test ./...
 else
     echo "amd64"
     export GOARCH=amd64
+
+    go test --race ./...
 fi
+
+export CGO_ENABLED=0
 
 go build --ldflags="-X pkg.version=$(git describe --dirty)"
